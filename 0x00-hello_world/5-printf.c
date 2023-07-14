ebcf22eb0a3242253f9df0d21326d99c6fe90870
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-14 13:21:47 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 /**
- *main - for printf function
- *return:always 0(success)
- */
+ *main - entry point
+ *
+ *description : print phrase by printf function
+ *
+ * return:always 0(success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+}
