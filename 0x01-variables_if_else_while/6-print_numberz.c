d75ddbc76063abb3750e1419eed4fcf59a2f72d1
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-15 12:24:33 -0700

    if

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print phrase by putchar function
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+int a = 48;
+for (a = 48 ; a <= 57 ; a++)
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}
+
