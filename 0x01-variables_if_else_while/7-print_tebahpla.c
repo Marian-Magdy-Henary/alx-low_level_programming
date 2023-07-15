efc5aaf05c32c37bf0ad2863c23bbffb61e66a98
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-15 12:37:18 -0700

    if

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print phrase by putchar function
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+int a = 122;
+for (a = 122 ; a >= 97 ; a--)
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}
