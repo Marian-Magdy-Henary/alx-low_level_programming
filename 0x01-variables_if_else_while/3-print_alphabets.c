f424f1c00ece2e052fa631b581c02e308adae822
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-15 11:28:22 -0700

    if

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print phrase by putchar function
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+int a = 97;
+int A = 65;
+for (a = 97 ; a <= 122 ; a++)
+{
+for (A = 65 ; A <= 90 ; A++)
+{
+putchar (A);
+}
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}
+
