c91f6c819eef9ed00e367234687f69a903397800
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-15 12:13:40 -0700

    if

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print phrase by putchar function
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+int a = 0;
+for (a = 0 ; a <= 9 ; a++)
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}
+
