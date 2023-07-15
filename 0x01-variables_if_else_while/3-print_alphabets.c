04766ab9116c6e4c321e539a6e89bd0e1bfedb0c
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-15 11:33:48 -0700

    if

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,12 +12,12 @@ int a = 97;
 int A = 65;
 for (a = 97 ; a <= 122 ; a++)
 {
-for (A = 65 ; A <= 90 ; A++)
-{
-}
 putchar (a);
 }
+for (A = 65 ; A <= 90 ; A++)
+{
 putchar (A);
+}
 putchar ('\n');
 return (0);
 }
