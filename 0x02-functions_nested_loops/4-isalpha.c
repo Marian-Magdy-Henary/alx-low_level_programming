e5ad5d71a9e255b35bee044228eef560b681215f
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-18 13:10:27 -0700

    loap

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - entry point
+ *
+ * @c : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+int _isalpha(int c)
+{
+if (c >= 97 &&  c <= 122 c >= 65 &&  c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
