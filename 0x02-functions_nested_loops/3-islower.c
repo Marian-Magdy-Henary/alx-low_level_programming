bc970f8c6c14f45845102d1399a85095e323c9d3
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-18 12:56:27 -0700

    loap

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - entry point
+ *
+ * @c : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+int _islower(int c)
+{
+if (c >= 97 &&  c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+{
+}
+
