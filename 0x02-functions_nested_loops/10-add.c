55d8d47ae208d6bbd0ed4985e74d62f0bba4ab54
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-19 15:14:51 -0700

    loap

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - entry point
+ *
+ * @a : checks input of function
+ *
+ * @b : checks input of function
+ *
+ * Return: 'sum' (success)
+*/
+int add(int a, int b)
+{
+int sum = a + b;
+return (sum);
+}
