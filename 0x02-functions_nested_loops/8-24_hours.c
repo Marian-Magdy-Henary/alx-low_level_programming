52545a2e6b880cea7729d9c50280f81029cd299a
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-19 11:38:45 -0700

    loap

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - entry point
+ *
+*/
+void jack_bauer(void)
+{
+int a = 48;
+int b = 48;
+int c = 48;
+char min = "";
+for (c = 48 ; c <= 56 ; c++)
+{
+for (d = 48 ; d <= 56 ; d++)
+{
+for (a = 48 ; a <= 56 ; a++)
+{
+for (b = 48 ; b <= 56 ; b++)
+{
+
+}
+}
+}
+}
+_putchar (c, d, 58, a, b);
+}
