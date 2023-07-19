9a1203217933d52db3316525e605184ea06b208e
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-19 12:26:01 -0700

    loap

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,22 +8,18 @@ void jack_bauer(void)
 int a = 48;
 int b = 48;
 int c = 48;
-int d = 48;
-for (c = 48 ; c <= 57 ; c++)
-{
-for (d = 48 ; d <= 50 ; d++)
+for (c = 0 ; c <= 23 ; c++)
 {
 for (a = 48 ; a <= 53 ; a++)
 {
 for (b = 48 ; b <= 57 ; b++)
 {
-_putchar (c);
-_putchar (d);
+_putchar (c / 10);
+_putchar (c % 10);
 _putchar (58);
 _putchar (a);
 _putchar (b);
 _putchar ('\n');
-}
 }
 }
 }
