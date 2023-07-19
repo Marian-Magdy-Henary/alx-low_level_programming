9b57de4b1d4199b9794862236b5a735c6d918b3c
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-19 14:40:18 -0700

    loap

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,8 +14,15 @@ for (b = 1 ; b <= 9 ; b++)
 _putchar (',');
 _putchar (' ');
 c = a * b;
-_putchar (c / 10);
-_putchar (c % 10);
+if (c <= 9)
+{
+_putchar (' ');
+}
+else
+{
+_putchar (c / 10 + 48);
+_putchar (c % 10 + 48);
+}
 }
 _putchar ('\n');
 }
