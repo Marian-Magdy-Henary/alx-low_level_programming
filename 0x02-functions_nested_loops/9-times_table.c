7c7b8bfbc373e712865f872bf42a84a528136931
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-19 13:30:24 -0700

    loap

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - entry point
+ *
+*/
+void times_table(void)
+{
+int a = 0;
+int b = 0;
+for (a = 0 ; a <= 9 ; a++)
+{
+for (b = 0 ; b <= 9 ; b++)
+{
+i = i + 1;
+_putchar (i);
+_putchar (',');
+_putchar (0);
+}
+}
+_putchar ('\n');
+}
