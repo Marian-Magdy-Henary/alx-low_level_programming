6359636939c704c483d6387ccc891531c2261558
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-19 14:05:38 -0700

    loap

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,15 +7,17 @@ void times_table(void)
 {
 int a = 48;
 int b = 48;
+int c = 0;
 for (a = 48 ; a <= 57 ; a++)
 {
+_putchar (48);
 for (b = 48 ; b <= 57 ; b++)
 {
-}
-}
-_putchar (a);
-_putchar (b);
+c = a * b;
+_putchar (c);
 _putchar (',');
 _putchar (0);
+}
+}
 _putchar ('\n');
 }
