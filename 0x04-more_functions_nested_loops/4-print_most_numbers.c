3add2d6ef5c1f5681a0ddc541af53d79beb4641a
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-21 09:20:10 -0700

    loap

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - entry point
+ *
+*/
+void print_most_numbers(void)
+{
+int i = 48;
+for (i = 48; i <= 57; i++)
+{
+if (i == 50 || i == 52)
+{
+_putchar (0);
+}
+_putchar (i + '\n');
+}
+}
