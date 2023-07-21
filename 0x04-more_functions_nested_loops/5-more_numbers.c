8746ad16033aff9639b710a59a28ca2880c7c568
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-21 11:34:03 -0700

    loap

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - entry point
+ *
+*/
+void more_numbers(void)
+{
+int i = 0;
+int c = 48;
+int d = 48;
+for (i = 0; i <= 9; i++)
+{
+for (c = 48; c <= 57; c++)
+{
+if (c == 57)
+{
+for (d = 48; d <= 52; d++)
+{
+_putchar (c);
+_putchar (49 + d);
+}
+}
+}
+}
+_putchar('\n');
+}
