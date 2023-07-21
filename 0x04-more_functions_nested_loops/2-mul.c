adb5d224fcc51e505b99bc3d65b5e8f300179f9b
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-21 08:43:40 -0700

    loap

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - entry point
+ *
+ * @a : input of function
+ *
+ * @b : checks input of function
+ *
+ * Return: always  'c' (success)
+*/
+int mul(int a, int b)
+{
+int c = a * b;
+return (c);
+}
