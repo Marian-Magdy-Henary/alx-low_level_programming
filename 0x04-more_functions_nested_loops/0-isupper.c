a4fe008a5fe158c71accb8d20cd007deff2bcde1
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-21 07:20:25 -0700

    loap

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - entry point
+ *
+ * @c : checks input of function
+ *
+ * Return: always  1 if 'c' is upper otherwise 0 (success)
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+if else(c >= 97 && c <= 122)
+{
+return (0);
+}
+}
