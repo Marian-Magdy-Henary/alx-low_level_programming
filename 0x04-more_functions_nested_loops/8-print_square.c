86afbe35c7d0c7a6b9b7f02dbbdd2a42dc5c627e
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-23 04:04:25 -0700

    loap

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - entry point
+ *
+ * @size : checks input of function
+*/
+void print_square(int size);
+{
+int raw = 1;
+int col = 1;
+if (size > 0)
+{
+for (raw = 1; raw <= size; raw++)
+{
+_putchar (35);
+for (col = 1; col <= raw; col++)
+{
+_putchar (35);
+}
+}
+_putchar ('\n');
+}
+else
+{
+_putchar ('\n');
+}
+}
+
