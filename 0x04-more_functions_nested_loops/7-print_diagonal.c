8406d5fc37cdaf715c75a7e9f0b6e0cee3bf57b0
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-23 02:13:20 -0700

    loap

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - entry point
+ *
+ * @n : checks input of function
+*/
+void print_diagonal(int n)
+{
+int i = 1;
+if (n > 0)
+{
+for (i = 1; i <= n; i++)
+{
+_putchar (92);
+}
+_putchar ('\n');
+}
+else
+{
+_putchar ('\n');
+}
+}
