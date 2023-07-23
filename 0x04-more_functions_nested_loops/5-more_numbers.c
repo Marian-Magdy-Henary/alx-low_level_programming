3dfb057af25019564d12536dd2390133b816c7be
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-23 02:52:57 -0700

    loap

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,7 @@ for (i = 0; i <= 9; i++)
 for (c = 48; c <= 57; c++)
 {
 _putchar (c);
-if (c == 57)
-{
-_putchar (49 + 48);
-}
-for (d = 49; d <= 52; d++)
+for (d = 48; d <= 52; d++)
 {
 _putchar (49 + d);
 }
