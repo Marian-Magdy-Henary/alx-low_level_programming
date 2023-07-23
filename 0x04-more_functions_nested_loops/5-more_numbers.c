31c9074ebd6781b1570db9553d55e12223c32bd4
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-23 02:59:02 -0700

    loap

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,18 @@
 */
 void more_numbers(void)
 {
-int i = 0;
+int i = 1;
 int c = 48;
 int d = 48;
-for (i = 0; i <= 9; i++)
+for (i = 1; i <= 10; i++)
 {
 for (c = 48; c <= 57; c++)
 {
+_putchar (c);
+}
 for (d = 48; d <= 52; d++)
 {
-_putchar (c);
 _putchar (49 + d);
-}
 }
 _putchar ('\n');
 }
