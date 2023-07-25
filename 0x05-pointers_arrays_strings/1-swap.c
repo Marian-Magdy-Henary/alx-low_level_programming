6772071c00418dde252e9498bfd7d5e1ef2269e7
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-25 07:22:37 -0700

    push

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - entry point
+ *
+ * @a : checks input of function
+ *
+ * @b : checks input of function
+*/
+void swap_int(int *a, int *b)
+{
+*a=42;
+*b=98;
+}
