a3f3d141649a74ae41342909c44f2f75167f6213
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-25 06:33:40 -0700

    push

--- 0-reset_to_98.c
+++ 0-reset_to_98.c
@@ -2,11 +2,12 @@
 /**
  * reset_to_98 - entry point
  *
- * @*n : checks input of function
+ * @n : checks input of function
 */
 void reset_to_98(int *n)
 {
 int i = 402;
-int *n = &i;
-_putchar ("n="+&i);
+*n = 98;
+*n = &i;
+_putchar ("n=" + &i);
 }
