ac7da5637429d086f1069a58ffe6789cd2f6a876
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-25 10:20:07 -0700

    push

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - entry point
+ *
+ * @s : checks input of function
+*/
+void print_rev(char *s)
+{
+int counter;
+for (counter = 0; *s == '\0'; s--)
+{
+_putchar (*s);
+counter++;
+}
+_putchar ('\n');
+}
