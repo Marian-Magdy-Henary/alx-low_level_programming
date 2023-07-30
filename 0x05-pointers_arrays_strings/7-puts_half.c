dbb3a3d8416eb82704771943a98ec47706e8a00e
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-30 12:10:21 -0700

    push

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,17 +8,10 @@ void puts_half(char *str)
 {
 int i = 1;
 int dev = 0;
-for (i = 1; str[i] != '\0'; i++)
+for (i = 0; str[i] != '\0'; i++);
+for (i /= 2; str[i] != '\0'; i++)
 {
-if (i % 2 == 0)
-{
-dev = i / 2;
-}
-else if (i % 2 != 0)
-{
-dev = (i - 1) / 2;
-}
-_putchar (str[dev]);
+_putchar (str[i]);
 _putchar ('\n');
 }
 }
