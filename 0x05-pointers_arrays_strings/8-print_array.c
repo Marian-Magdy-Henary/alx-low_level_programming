bbe606d7f7cac1bdf83f0f9600a4e91cbb4c4f67
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-30 13:13:48 -0700

    push

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,14 @@ void print_array(int *a, int n)
 int i = 0;
 for (i = 0; i < n &&  a[i] != '\0'; i++)
 {
-printf ("%d ",a[i]);
+printf ("%d",a[i]);
 if (n > i)
 {
-printf ("%s %s",",","");
+printf ("%s%s",",","");
+}
+else
+{
+printf ("%s","");
 }
 }
 }
