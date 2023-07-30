93671134bacfb0cd7520a1cd114cf92c95ef3de0
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-30 13:57:28 -0700

    push

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcpy - entry point
+ *
+ * @dest : checks input of function
+ *
+ * @src : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+int c = 0;
+for (i = 0; src[i] != '\0'; i++)
+;
+i++;
+for (c = 0; dest[c] != '\0'; c++)
+{
+dest[c] = src[i];
+}
+return (dest);
+}
