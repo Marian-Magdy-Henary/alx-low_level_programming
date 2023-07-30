9d8e387b165d5dbacdd4c627cfe77e4c3f6e1582
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-30 10:49:15 -0700

    push

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - entry point
+ *
+ * @str : checks input of function
+*/
+void puts_half(char *str)
+{
+int i = 0;
+int dev = 0;
+for (i = 0; str[i]; i++)
+{
+if (i % 2 == 0)
+{
+dev = i / 2;
+str[dev];
+}
+else if (i % 2 != 0)
+{
+dev = (i - 1) / 2;
+str[dev];
+}
+}
+}
