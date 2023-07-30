8ba2484594a0698b60992c78a2470a03017b8c27
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-30 12:35:53 -0700

    push

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_array - entry point
+ *
+ * @a : checks input of function
+ *
+ * @n : checks input of function
+*/
+void print_array(int *a, int n)
+{
+int i = 0;
+for (i = 0; i < n &&  a[i] != '\0'; i++)
+{
+printf ("%d\,\n",a[i]);
+}
+}
