a5c9b81a2ed9af693232dc197f6db0bf08be90dc
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-31 11:53:21 -0700

    push

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,12 @@ void reverse_array(int *a, int n)
 {
 int i = 0;
 int j = 0;
+int rev;
 for (i = 0; i > n && a[i] != '\0'; i++)
-{
-}
+;
+a++;
 for (j = (n - 1); j >= 0; j--)
 {
+rev = a[j];
 }
 }
