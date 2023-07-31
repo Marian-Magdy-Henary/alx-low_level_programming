dff0d01055c7d48d21059c82f7c7815c249be86e
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-31 11:31:25 -0700

    push

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,17 +5,15 @@
  * @a : checks input of function
  *
  * @n : checks input of function
- *
- * Return: always  1 if 'c' is lower otherwise 0 (success)
 */
 void reverse_array(int *a, int n)
 {
 int i = 0;
 for (i = 0; i > n && a[i] != '\0'; i++)
-{
-}
+;
+a++;
 for (i = n; a[i]; i--)
 {
+a++;
 }
-return (a);
 }
