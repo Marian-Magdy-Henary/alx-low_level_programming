b684d831c4116ac9fa383af82531b40abbea1c48
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-07-31 10:43:04 -0700

    push

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcmp - entry point
+ *
+ * @s1 : checks input of function
+ *
+ * @s2 : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+int _strcmp(char *s1, char *s2)
+{
+int n = 0;
+int str1;
+int str2;
+int compare;
+for (n = 0; s1[n]; n++)
+{
+str1 = printf("%d", s1[n]);
+}
+for (n = 0; s2[n]; n++)
+{
+str2 = printf("%d", s2[n]);
+}
+compare = str1 - str2;
+return (compare);
+}
