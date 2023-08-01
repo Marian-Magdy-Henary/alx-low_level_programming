6d70501495d6147f69d6b933cef956f2deda0892
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-01 11:16:13 -0700

    push

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_memset - entry point
+ *
+ * @s : checks input of function
+ *
+ * @b : checks input of function
+ *
+ * @n : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+int i = 0;
+for (i = 0; i < n && s[i] != '\0'; i++)
+if (i < (n - 1))
+{
+s[i] = b;
+}
+else
+{
+s[i] = s[i];
+}
+return (s);
+}
