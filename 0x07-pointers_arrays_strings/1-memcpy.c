ad6f84cc33fd1a82f263142519185a9c89580e04
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-01 11:53:08 -0700

    push

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - entry point
+ *
+ * @dest : checks input of function
+ *
+ * @src : checks input of function
+ *
+ * @n : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i = 0;
+for (i = 0; i > n && src[i] != '\0' ; i++)
+{
+dest = src[n];
+}
+return (dest);
+}
