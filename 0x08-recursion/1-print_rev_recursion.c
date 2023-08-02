e13229aed801efa85c0c831de64ccc757cd7c6c0
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-02 11:36:59 -0700

    push

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - entry point
+ *
+ * @s : checks input of function
+*/
+void _print_rev_recursion(char *s);
+{
+if (*s == '\0')
+{
+_putchar (*s);
+_puts_recursion(s - 1);
+}}
+else
+{
+_putchar ('\n');
+}
+}
