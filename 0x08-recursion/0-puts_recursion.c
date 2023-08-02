d4c4e0cd3bb00acb941f59a6a61e28cd8a666759
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-02 11:02:38 -0700

    push

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - entry point
+ *
+ * @s : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+void _puts_recursion(char *s)
+{
+_putchar (s);
+_putchar ('\n');
+}
