080297f792b90eb426b830a4ce373c375d752163
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-03 09:52:38 -0700

    push

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
  #include "main.h"
+ #include <stdio.h>
 /**
  * is_prime_number - entry point
  *
@@ -8,16 +9,26 @@
 */
 int is_prime_number(int n)
 {
-if (n % n != 0 || n % 1 != 0)
+return (add(n, 2));
+}
+/**
+ * add- entry point
+ *
+ * @x : checks input of function
+ *
+ * @y : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+int add(int x, int y)
+{
+if (y >= x || n > 1)
 {
 return (1);
 }
-if (n == 0 || n == 1)
+else if (n % y == 0 || n > 1)
 {
 return (0);
 }
-else if (n % n == 0 || n % 1 == 0)
-{
-return (0);
-}
+return (add(x, y + 1));
 }
