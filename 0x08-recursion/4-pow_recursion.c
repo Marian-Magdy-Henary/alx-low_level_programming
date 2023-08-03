d1c5dc0d90dfb52b93a30f58c67d3ce7aa192b96
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-03 08:26:49 -0700

    push

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,14 +10,13 @@
 */
 int _pow_recursion(int x, int y)
 {
-int res = 0;
 if (y < 0)
 {
 return (-1);
 }
-else if (y > 0)
+if (y > 0)
 {
-res = x * _pow_recursion(x, y - 1);
+int res = x * _pow_recursion(x, y - 1);
 }
 return (res);
 }
