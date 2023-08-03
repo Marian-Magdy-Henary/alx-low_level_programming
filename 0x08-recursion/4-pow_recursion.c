93581c09bda58cc5e448d265ea6c27fa89d4ea47
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-03 07:55:05 -0700

    push

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+ #include "main.h"
+/**
+ * _pow_recursion - entry point
+ *
+ * @x : checks input of function
+ *
+ * @y : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else
+{
+return (_pow_recursion(int x, int y + 1);
+}
+}
