feed6a56a99fdaa2fe9623057887217cea46f303
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-03 10:09:17 -0700

    push

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
  #include "main.h"
+#include <stdio.h>
 /**
  * _sqrt_recursion - entry point
  *
@@ -8,13 +9,29 @@
 */
 int _sqrt_recursion(int n)
 {
-if (n % 10 != 0 || n < 0)
+return (add(n, 1));
+}
+/**
+ * add - entry point
+ *
+ * @n : checks input of function
+ *
+ * @x : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+int add(int n, int x)
+{
+if (x * x == n)
+{
+return (x);
+}
+else if (x * x != n)
+{
+return (add(n, x + 1));
+}
+else
 {
 return (-1);
 }
-if (n % 10 == 0 && n > 0)
-{
-n /= _sqrt_recursion(int n)
-}
-return (n);
 }
