ffd32667dcb781e9e3077816c3000716ddb9abce
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-04 12:49:01 -0700

    push

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * @s : checks input of function
+ *
+ * @x : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+int main(int x, char const *s[])
+{
+(void)x;
+printf("%d\n", x);
+return (0);
+}
+
