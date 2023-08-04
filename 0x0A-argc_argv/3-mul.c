a0e423099bf3a12709e5ac898e261ebf81fee4a7
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-04 13:23:51 -0700

    push

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * @s : checks input of function
+ *
+ * @x : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+int main(int x, char const *s[])
+{
+int res, num1, num2;
+num1 = _atoi(s[1]);
+num2 = _atoi(s[2]);
+res = num1 + num2;
+printf("%d\n", res);
+return (0);
+if (x > 3 || x < 3)
+{
+printf("%s\n", "Error");
+}
+}
+
