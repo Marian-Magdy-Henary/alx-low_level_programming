f23a382d3980b718a889cac449fcddfec40ee4a7
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-05 12:22:09 -0700

    push

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * @s : checks input of function
+ *
+ * @x : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+int main(int x, char const *s[])
+{
+int i = 0;
+int res = 0;
+for (i = 0; i < x; i++)
+{
+if (s[i] <= 48 || s[i] <= 57)
+{
+res += atoi(s[i]);
+printf("%d\n", res);
+}
+else if (s[0])
+{
+printf("0\n");
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+return (0);
+}
