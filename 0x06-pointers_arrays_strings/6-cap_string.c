1cf4c27a52bb340e4cb89001bfe06ab31e79acaf
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-08 08:40:54 -0700

    push

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,14 +11,14 @@ char *cap_string(char *str)
 int i = 0;
 for (i = 0; str[i] != '\0' ; i++)
 {
-if (str[i] == 32 || str[i] == '\n'
-|| str[i] == 144 || str[i] ==  59 || str[i] == 33
-|| str[i] == 46 || str[i] ==  63 || str[i] == 34
-|| str[i] == 40 || str[i] ==  41
-|| str[i] ==  125 || str[i] == 123)
-&& (str[i + 1] >= 97 && str[i + 1] <= 122)
+if (str[i - 1] == 32 || str[i - 1] == '\n'
+|| str[i - 1] == 144 || str[i - 1] ==  59 || str[i - 1] == 33
+|| str[i - 1] == 46 || str[i - 1] ==  63 || str[i - 1] == 34
+|| str[i - 1] == 40 || str[i - 1] ==  41
+|| str[i - 1] ==  125 || str[i - 1] == 123)
+&&(str[i] >= 97 && str[i] <= 122)
 {
-str[i + 1] = str[i + 1] - 32;
+str[i] = str[i] - 32;
 }
 }
 return (str);
