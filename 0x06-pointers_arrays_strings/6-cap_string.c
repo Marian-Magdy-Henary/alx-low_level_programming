bfc433920d0ae0250cb9574a12d4d108de764935
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-08 08:52:57 -0700

    push

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,15 +11,24 @@ char *cap_string(char *str)
 int i = 0;
 for (i = 0; str[i] != '\0' ; i++)
 {
-if (str[i - 1] == 32 || str[i - 1] == '\n'
-|| str[i - 1] == 144 || str[i - 1] ==  59 || str[i - 1] == 33
-|| str[i - 1] == 46 || str[i - 1] ==  63 || str[i - 1] == 34
-|| str[i - 1] == 40 || str[i - 1] ==  41
-|| str[i - 1] ==  125 || str[i - 1] == 123)
-&&(str[i] >= 97 && str[i] <= 122)
+if (str[i - 1] == ' '||
+str[i - 1] == '\n'|| 
+str[i - 1] == '\t'||
+str[i - 1] == ','|| 
+str[i - 1] == ';'|| 
+str[i - 1] == '.'|| 
+str[i - 1] == '!'|| 
+str[i - 1] == '?'|| 
+str[i - 1] == '"'|| 
+str[i - 1] ==  '('|| 
+str[i - 1] ==  ')'|| 
+str[i - 1] == '{' ||
+str[i - 1] == '}'&&
+str[i] >= 97 && str[i] <= 122)
 {
 str[i] = str[i] - 32;
 }
+}
 return (str);
 }
-}
+
