c734fc76b34046183d445e0bed1b9c45b773cda7
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-11 04:52:00 -0700

    push

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn- entry point
+ *
+ * @s : checks input of function
+ *
+ * @accept : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i = 0;
+int x = 0;
+for (i = 0; s[i] >= '\0'; i++)
+{
+for (x = 0; accept[x] >= '\0' ; x++)
+{
+if (s[i] == accept[x])
+{
+break;
+return (s++);
+}
+else
+{
+return (NULL);
+}
+}
+}
+return (0);
+}
