8be63dc0dded1ea2b55e022cc8e5221ee7bd1b42
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-11 03:52:43 -0700

    push

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn- entry point
+ *
+ * @s : checks input of function
+ *
+ * @accept : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+int i = 0;
+int x = 0;
+int count = 0;
+for (i = 0; s[i] >= '\0'; i++)
+{
+for (x = 0; accept[x] >= '\0' ; x++)
+{
+if (s[i] == accept[x])
+{
+count++;
+return (count++);
+}
+}
+}
+return (0);
+}
+
