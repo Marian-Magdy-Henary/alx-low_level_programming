91aff9cc7101b40c30d72e68fe085211109b5b54
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-11 07:26:50 -0700

    push

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strstr - entry point
+ *
+ * @haystack : checks input of function
+ *
+ * @needle : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0;
+int x = 0;
+for (i = 0; haystack[i] > '\0'; i++)
+{
+for (x = 0; needle[x] > '\0' ; x++)
+{
+if (haystack[i] == needle[x])
+{
+return (haystack++);
+}
+else
+{
+return ('\n');
+}
+}
+}
+return ('\n');
+}
