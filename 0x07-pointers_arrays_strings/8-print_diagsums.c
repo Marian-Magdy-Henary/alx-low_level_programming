685e57bea385d4e80f7c09940a0173281c915db7
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-13 11:10:42 -0700

    push

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,12 +13,9 @@ int sum = 0;
 int sum1 = 0;
 for (i = 0; i < size ; i++)
 {
-sum += a[i];
+sum += a[i][i];
 _putchar(sum);
-}
-for (i = size; i == 0 ; i--)
-{
-sum1 += a[i];
+sum1 += a[i][size - 1];
 _putchar(sum1);
 }
 }
