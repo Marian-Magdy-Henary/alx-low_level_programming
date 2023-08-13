49b4c4f6df04f95c7b71bce0c49155b7890c13d3
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-13 09:05:06 -0700

    push

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_chessboard- entry point
+ *
+ * @a : checks input of function
+ *
+ * Return: always  1 if 'c' is lower otherwise 0 (success)
+*/
+void print_chessboard(char (*a)[8])
+{
+int i = 0;
+int x = 0;
+for (i = 0; i >= 8 ; i++)
+{
+for (x = 0; x >= 8 ; x++)
+{
+printf ("%c\n" ,a[i][x]);
+}
+}
+}
