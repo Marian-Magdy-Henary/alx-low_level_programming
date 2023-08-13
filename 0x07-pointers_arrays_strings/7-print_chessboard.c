9b17dfd34ee0061813dfb30c0e5ad939eaebc060
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-13 09:17:49 -0700

    push

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,12 +8,12 @@ void print_chessboard(char (*a)[8])
 {
 int i = 0;
 int x = 0;
-for (i = 0; i >= 7 ; i++)
+for (i = 0; i < 8 ; i++)
 {
-for (x = 0; x >= 7 ; x++)
+for (x = 0; x < 8 ; x++)
 {
 _putchar(a[i][x]);
 }
-_putchar("\n");
+_putchar('\n');
 }
 }
