674efa98d7585dbf271e261d11047368593ed2e9
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-13 11:01:33 -0700

    push

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagsums- entry point
+ *
+ * @a : checks input of function
+ *
+ * @size : checks input of function
+*/
+void print_diagsums(int *a, int size)
+{
+int i = 0;
+int sum = 0;
+int sum1 = 0;
+for (i = 0; i < size ; i++)
+{
+sum += a[i];
+_putchar(sum);
+}
+_putchar(',');
+_putchar(' ');
+for (i = size; i == 0 ; i--)
+{
+sum1 += a[i];
+_putchar(sum1);
+}
+}
