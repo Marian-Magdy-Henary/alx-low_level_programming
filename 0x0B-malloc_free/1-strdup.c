9fd4e2f08625896539eaa80bf058399f1c37144f
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-15 12:25:40 -0700

    shell

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ if (str == NULL)
 return (NULL);
 }
 for (size = 0; str[size] != '\0' ; size++)
-{
+;
 n = malloc(size * sizeof(*str) + 1);
 if (n == 0)
 {
@@ -27,8 +27,7 @@ else
 {
 for (i = 0 ; i < size; i++)
 {
-n[i + 1] = str[i + 1];
-}
+n[i] = str[i];
 }
 }
 return (n);
