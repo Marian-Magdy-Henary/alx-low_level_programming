5830a6d1574affd307e6d751d8e5a1390927d8b3
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-15 11:19:26 -0700

    shell

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,12 @@
 char *create_array(unsigned int size, char c)
 {
 unsigned int i = 0;
-char *n = (char*) malloc(size);
+char *n = (char *)malloc(size);
 if (size == 0 || n == 0)
 {
 return (NULL);
 }
-for (i = 0; i <=size; i++)
+for (i = 0; i <= size; i++)
 {
 n[i] = c;
 }
