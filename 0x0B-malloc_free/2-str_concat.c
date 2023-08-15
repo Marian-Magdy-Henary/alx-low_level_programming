83517547db7060fbc2f0c528f05c6a1c00e9efe8
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2023-08-15 13:11:25 -0700

    shell

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,23 +13,28 @@ char *str_concat(char *s1, char *s2)
 {
 int i = 0;
 int x = 0;
+int z = 0;
 int size1;
 int size2;
 char *n;
-size1 = sizeof(s1);
-size2 = sizeof(s2);
-n = malloc((size1  + size2)*sizeof(char) + 1));
+for (x = 0; s1[x] != '\0'; x++)
+;
+for (z = 0; s2[z] != '\0'; z++)
+;
+size1 = x;
+size2 = z;
+n = malloc((size1  + size2) * sizeof(char) + 1);
 if (n == 0)
 {
 return (0);
 }
 if (s1 == NULL)
 {
-s1 = '\n';
+s1 = '\0';
 }
-if (S2 == NULL)
+if (s2 == NULL)
 {
-s2 = '\n';
+s2 = '\0';
 }
 for (i = 0; i <= size1 + size2 ; i++)
 {
@@ -39,7 +44,7 @@ n[i] = s1[i];
 }
 else
 {
-n[i] = s[i- size1];
+n[i] = s2[i- size1];
 }
 }
 n[i] = '\0';
