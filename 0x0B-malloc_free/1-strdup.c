525ce8550d54fda1a6b77683b2d88a6f1d758e78
Marian-Magdy-Henary <marianmgdy8989@gmail.com>
2024-02-09 02:42:06 -0800

    f

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,27 +9,22 @@
 */
 char *_strdup(char *str)
 {
-int i = 0;
+int j = 0;
 int size = 0;
-char *n;
+char *X  = NULL;
+for (size = 0; str[size] != '\0' ; size++)
+{
+}
+X = malloc(size * sizeof(char) + 1);
+for (j = 0 ; j < size ; j++)
+{
+X[j] = str[j];
+}
+return (X);
 if (str == NULL)
 {
 return (NULL);
 }
-for (size = 0; str[size] != '\0' ; size++)
-;
-n = malloc(size * sizeof(*str) + 1);
-if (n == 0)
-{
-return (NULL);
 }
-else
-{
-for (i = 0 ; i < size; i++)
-{
-n[i] = str[i];
-}
-}
-return (n);
-}
+
 
